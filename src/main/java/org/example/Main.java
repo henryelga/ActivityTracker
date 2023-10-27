1eb1545e360171ac6c0b145475ad39a3ab399d64
milamocha <d00253640@student.dkit.ie>
2023-10-27 12:43:39 +0100

    read in CSV file (no sample data inserted yet)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,32 @@
 package org.example;
+import java.util.Scanner;
+import java.io.*;
+
+import java.io.FileNotFoundException;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main
-{
-    public static void main(String[] args)
-    {
-        System.out.print("Hello and welcome!");
+public class Main {
+    public static void main(String[] args) {
+        String fileName = "ActivityData.csv";
+
+        try (Scanner sc = new Scanner(new File("ActivityData.csv")))
+        {
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                String[] tokens = line.split(",");
+
+                String activity_type = tokens[0];
+                double duration = Double.parseDouble(tokens[1]);
+                String date = tokens[2];
+                double distance = Double.parseDouble(tokens[3]);
+                int heart_rate = Integer.parseInt(tokens[4]);
+
+                System.out.printf("%-20s %5.2f %10s %5.2f %3d %n", activity_type, duration, date, distance, heart_rate);
+            }
+
+        } catch (FileNotFoundException exception) {
+            System.out.println("FileNotFoundException caught. The file " + fileName + "may not exist." + exception);
+        }
     }
 }
