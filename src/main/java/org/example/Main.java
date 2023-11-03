b68710e60635ecb12315991f027cfb7e3a1fe1f4
milamocha <d00253640@student.dkit.ie>
2023-11-03 01:00:31 +0000

    fixed formatting

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,6 +9,7 @@ public class Main {
         // Read in the data from the CSV
         String fileName = "ActivityData.csv";
 
+        System.out.printf("%-16s %-12s %-10s %-10s %-10s %n", "Activity Type", "Date", "Duration", "Distance", "BPM"); // temporary line location
         try (Scanner sc = new Scanner(new File("ActivityData.csv")))
         {
             while (sc.hasNextLine()) {
@@ -21,7 +22,7 @@ public class Main {
                 double distance = Double.parseDouble(tokens[3]);
                 int heart_rate = Integer.parseInt(tokens[4]);
 
-                System.out.printf("%-20s %10s %5.2f %5.2f %3d %n", activity_type, date, duration, distance, heart_rate);
+                System.out.printf("%-16s %-12s %-10.2f %-10.2f %-10d %n", activity_type, date, duration, distance, heart_rate);
             }
 
         } catch (FileNotFoundException exception) {
