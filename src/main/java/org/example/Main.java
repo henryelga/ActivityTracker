a4d70bb59f8ff74eef8ccd4e5711b301eed367f9
milamocha <d00253640@student.dkit.ie>
2023-11-03 00:25:56 +0000

    sample data in CSV file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,8 +4,6 @@ import java.io.*;
 
 import java.io.FileNotFoundException;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         String fileName = "ActivityData.csv";
